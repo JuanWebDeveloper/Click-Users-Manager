fb928f67909c9f7a88b4dee3024b230205908de4
Juan Vargas <juandeveloper19@gmail.com>
2024-04-29 17:47:53 -0500

    I Added 'create_user' command to CLI
    A new command, 'create_user', has been implemented and added to the command line interface. This command allows the creation of a new user with 'name', 'lastname', and 'age' options. The command is defined in the 'commands' folder, specifically in the 'create_user_command.py' file. The command reads existing data from a JSON file, appends the new user data, and writes the updated data back to the JSON file.

---
+++ commands/create_user_command.py
@@ -0,0 +1,24 @@
+import click
+from utils.json_data_manager import read_data_from_json
+from utils.json_data_manager import write_data_to_json
+
+# Define the 'create_user' command
+@click.command("create_user")
+# Add options for the command
+@click.option('--name', required=True, help='Name of the user')
+@click.option('--lastname', required=True, help='Lastname of the user')
+@click.option('--age', required=True, help='Age of the user')
+def create_user(name, lastname, age):
+	 # Read the existing data from the JSON file
+    data = read_data_from_json()
+    # Append the new user to the data
+    data.append({
+        'id': len(data) + 1,
+        'name': name,
+        'lastname': lastname,
+        'age': int(age)
+    })    
+    # Write the updated data back to the JSON file
+    write_data_to_json(data)
+    # Print a success message
+    print(f"User {name} {lastname} added successfully with id {len(data)}")
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import click
 from commands.list_users_command import list_users
+from commands.create_user_command import create_user
 
 @click.group()
 def command_line_interface():
     pass
 
-# Adding the 'list_users' command to the command line interface
+# Adding commands to the command line interface
 command_line_interface.add_command(list_users)
+command_line_interface.add_command(create_user)
 
 if __name__ == '__main__':
     command_line_interface()
